ed28b1a32f5813beea852c6fad47c28788098a09
nikzhechev <nikolayzhechev87@gmail.com>
2021-11-25 23:04:32 +0200

    Play whit Java

---
+++ JavaExample2/src/Task1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 100) {
+            System.out.println(number + " ");
+            number++;
+        }
+
+
+    }
+}
